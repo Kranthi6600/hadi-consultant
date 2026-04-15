71193f50e4c3a2adc590177380c62b12c78510ed
kranthi <sriramkranthikumar7672@gmail.com>
2026-04-15 06:27:03 +0530

    Fix Next.js layout issues - move CSS imports to globals.css and remove Head component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,4 @@
-import Head from 'next/head';
-import "../../public/assets/css/plugins/swiper.min.css";
-import "../../public/assets/css/plugins/fontawesome-5.css";
-import "../../public/assets/css/plugins/animate.min.css";
-import "../../public/assets/css/plugins/unicons.css";
-
-import "../../public/assets/css/vendor/bootstrap.min.css";
-import "../../public/assets/css/style.css";
-
+import "../globals.css";
 
 export const metadata = {
   title: "Hadi Consultant Business",
@@ -19,19 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Hadi Consultant Business</title>
-        <meta name="author" content="Hadi Consultant" />
-        <meta name="description" content="Hadi Consultant Business" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/assets/images/cropped-cropped-HADI_CONSULTANTS_logo-removebg-preview-140x74.ico" />
-      </Head>
       <body className='index-one'>
-
         {children}
-
-
-
       </body>
     </html>
   );
